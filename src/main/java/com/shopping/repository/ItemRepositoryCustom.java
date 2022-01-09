a3fc32f8537b4a10f4360a4ae282f60dd3d14e86
SeongJin Oh <guk0230@naver.com>
2022-01-09 22:57:09 +0900

    메인화면(ItemRepositoryCustom.java)

--- src/main/java/com/shopping/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shopping/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shopping.repository;
 
 import com.shopping.dto.ItemSearchDto;
+import com.shopping.dto.MainItemDto;
 import com.shopping.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,7 @@ public interface ItemRepositoryCustom {
     //상품 조회 조건을 담고 있는 itemSearchDto 객체와 페이징 정보를 담고 있는 pageable 객체를 파라미터로 받기
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 
+    //메인 페이지에 보여줄 상품 리스트를 가져오는 메소드
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto,Pageable pageable);
+    
 }
