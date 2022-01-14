5b7c9af12ed7bb2213687378ca47ead427bd7a03
SeongJin Oh <guk0230@naver.com>
2022-01-14 22:38:37 +0900

    주문 이력 조회(ItemImgRepository.java)

--- src/main/java/com/shopping/repository/ItemImgRepository.java
+++ src/main/java/com/shopping/repository/ItemImgRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 //상품의 이미지 정보를 저장하기 위해
 public interface ItemImgRepository extends JpaRepository<ItemImg,Long> {
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+    //상품의 대표 이미지를 찾는 쿼리 메소드 추가
+    ItemImg findByItemIdAndRepimgYn(Long itemId,String repimgYn);
+
 }
