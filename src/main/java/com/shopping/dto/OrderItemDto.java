f3c3bf47843a1adfc52e82c85a4438359cefe694
SeongJin Oh <guk0230@naver.com>
2022-01-14 16:59:55 +0900

    주문 이력 조회(OrderItemDto.java)

---
+++ src/main/java/com/shopping/dto/OrderItemDto.java
@@ -0,0 +1,26 @@
+package com.shopping.dto;
+
+import com.shopping.entity.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+//주문 상품 정보를 담을 Dto
+@Getter @Setter
+public class OrderItemDto {
+
+    public OrderItemDto(OrderItem orderItem,String imgUrl) {
+        this.itemNm=orderItem.getItem().getItemNm();
+        this.count=orderItem.getCount();
+        this.orderPrice=orderItem.getOrderPrice();
+        this.imgUrl=imgUrl;
+    }
+
+    private String itemNm; //상품명
+
+    private int count; //주문 수량
+
+    private int orderPrice; //주문 금액
+
+    private String imgUrl; //상품 이미지 경로
+
+}
