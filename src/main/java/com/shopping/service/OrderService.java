20ac68c7fdfc4a984ac562f235a9aa2cf7a8d8a7
SeongJin Oh <guk0230@naver.com>
2022-01-15 17:04:42 +0900

    주문 이력 조회(OrderService.java)

--- src/main/java/com/shopping/service/OrderService.java
+++ src/main/java/com/shopping/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shopping.service;
 
 import com.shopping.dto.OrderDto;
-import com.shopping.entity.Item;
-import com.shopping.entity.Member;
-import com.shopping.entity.Order;
-import com.shopping.entity.OrderItem;
+import com.shopping.dto.OrderHistDto;
+import com.shopping.dto.OrderItemDto;
+import com.shopping.entity.*;
+import com.shopping.repository.ItemImgRepository;
 import com.shopping.repository.ItemRepository;
 import com.shopping.repository.MemberRepository;
 import com.shopping.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto,String email){
 
@@ -40,6 +44,29 @@ public class OrderService {
 
         return order.getId();
 
+    }
+
+    //주문 목록을 조회하는 로직 구현
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable){
+
+        List<Order> orders=orderRepository.findOrders(email,pageable); //유저의 아이디와 페이징 조건을 이용하여 주문 목록 조회
+        Long totalCount=orderRepository.countOrder(email); //유저의 주문 총 개수 구하기
+
+        List<OrderHistDto> orderHistDtos=new ArrayList<>();
+        for(Order order:orders){
+            OrderHistDto orderHistDto=new OrderHistDto(order);
+            List<OrderItem> orderItems=order.getOrderItems();
+            for(OrderItem orderItem:orderItems){
+                ItemImg itemImg=itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(),"Y"); //주문한 상품 대표 이미지 조회
+                OrderItemDto orderItemDto=new OrderItemDto(orderItem,itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos,pageable,totalCount);
     }
 
 }
