1191c8a5ce699a4309f1d86b571f1313d3fdee4c
SeongJin Oh <guk0230@naver.com>
2022-01-15 17:14:52 +0900

    주문 이력 조회(OrderController.java)

--- src/main/java/com/shopping/controller/OrderController.java
+++ src/main/java/com/shopping/controller/OrderController.java
@@ -1,20 +1,24 @@
 package com.shopping.controller;
 
 import com.shopping.dto.OrderDto;
+import com.shopping.dto.OrderHistDto;
 import com.shopping.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 //상품 주문에서 웹 페이지의 새로 고침 없이 서버에 주문을 요청하기 위해 비동기 방식 사용
 @Controller
@@ -47,4 +51,20 @@ public class OrderController {
         return new ResponseEntity<Long>(orderId,HttpStatus.OK); //결과값으로 생성된 주문 번호와 요청이 성공했다는 HTTP 응답 상태 코드 반환
         
     }
+
+    @GetMapping(value = {"/orders","/orders/{page}"})
+    public String orderHist(@PathVariable("page")Optional<Integer> page, Principal principal, Model model){
+        Pageable pageable= PageRequest.of(page.isPresent() ? page.get() : 0,4); //한번에 가지고 올 주문의 개수 4개로 설정
+
+        //현재 로그인한 회원은 이메일과 페이징 객체를 파라미터로 전달하여 화면에 전달한 주문 목록 데이터를 리턴 값으로 받는다.
+        Page<OrderHistDto> orderHistDtoList=orderService.getOrderList(principal.getName(),pageable);
+
+        model.addAttribute("orders",orderHistDtoList);
+        model.addAttribute("page",pageable.getPageNumber());
+        model.addAttribute("maxPage",5);
+
+        return "order/orderHist";
+
+    }
+
 }
