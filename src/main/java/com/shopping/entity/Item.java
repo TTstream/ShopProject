8bec5e900e88db687c7830f37e9d5e636a4ab41b
SeongJin Oh <guk0230@naver.com>
2022-01-16 14:50:06 +0900

    주문 취소하기(Item.java)

--- src/main/java/com/shopping/entity/Item.java
+++ src/main/java/com/shopping/entity/Item.java
@@ -52,4 +52,9 @@ public class Item extends BaseEntity{
         this.stockNumber=restStock; //주문 후 남은 재고 수량을 상품의 현재 재고 값으로 할당
     }
 
+    //상품 주문을 취소할 경우 상품의 재고를 더해주기 위한 로직
+    public void addStock(int stockNumber){
+        this.stockNumber+=stockNumber;
+    }
+
 }
