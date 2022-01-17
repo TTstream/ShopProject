ee3ed30555cae660a1dc212cb129c92b6788a4e2
SeongJin Oh <guk0230@naver.com>
2022-01-17 15:16:37 +0900

    장바구니 담기(CartRepository.java)

--- src/main/java/com/shopping/repository/CartRepository.java
+++ src/main/java/com/shopping/repository/CartRepository.java
@@ -4,5 +4,7 @@ import com.shopping.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    // 현재 로그인한 회원의 Cart 엔티티를 찾기 위해서 메소드 추가
+    Cart findByMemberId(Long memberId);
 
 }
