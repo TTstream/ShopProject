e7e100427f23b1a467041e8572ff1265a12586cd
SeongJin Oh <guk0230@naver.com>
2022-01-17 16:30:33 +0900

    장바구니 담기(CartController.java)

---
+++ src/main/java/com/shopping/controller/CartController.java
@@ -0,0 +1,52 @@
+package com.shopping.controller;
+
+import com.shopping.dto.CartItemDto;
+import com.shopping.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping(value = "/cart")
+    public @ResponseBody
+    ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto,
+                         BindingResult bindingResult, Principal principal){
+
+        if(bindingResult.hasErrors()){ //장바구니에 담을 상품 정보 에러가 있는지 검사
+            StringBuilder sb=new StringBuilder();
+            List<FieldError> fieldErrors=bindingResult.getFieldErrors();
+            for(FieldError fieldError: fieldErrors){
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        String email=principal.getName(); //현재 로그인한 회원의 이메일 정보를 변수에 저장
+        Long cartItemId;
+
+        try {
+            //화면으로부터 넘어온 장바구니에 담을 상품 정보와 현재 로그인한 회원의 이메일 정보를 이용하여 장바구니에 상품을 담는 로직 호출
+            cartItemId=cartService.addCart(cartItemDto,email);
+        }catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Long>(cartItemId,HttpStatus.OK);
+    }
+
+}
