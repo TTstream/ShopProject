1381b88114a75633acc1021336e8d1521b8779c6
SeongJin Oh <guk0230@naver.com>
2022-01-24 08:47:44 +0900

    장바구니 상품 주문(OrderService.java)

--- src/main/java/com/shopping/service/OrderService.java
+++ src/main/java/com/shopping/service/OrderService.java
@@ -91,4 +91,22 @@ public class OrderService {
         order.cancelOrder(); //주문 취소 상태로 변경하면 변경 감지 기능에 의해서 트랜잭션이 끝날 때 update 쿼리가 실행
     }
 
+    public Long orders(List<OrderDto> orderDtoList, String email){
+
+        Member member=memberRepository.findByEmail(email);
+        List<OrderItem> orderItemList=new ArrayList<>();
+
+        for(OrderDto orderDto:orderDtoList){ //주문할 상품 리스트 생성
+            Item item=itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);
+
+            OrderItem orderItem=OrderItem.createOrderItem(item,orderDto.getCount());
+            orderItemList.add(orderItem);
+        }
+
+        Order order=Order.createOrder(member,orderItemList); //현재 로그인한 회원과 주문 상품 목록 이용하여 주문 생성
+        orderRepository.save(order); //주문 데이터 저장
+
+        return order.getId();
+    }
+
 }
