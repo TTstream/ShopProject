4d15bd4ccc00802cbc9c7a8d37be28ca1fe80e99
SeongJin Oh <guk0230@naver.com>
2022-01-25 16:08:33 +0900

    장바구니 상품 주문(CartService.java)

--- src/main/java/com/shopping/service/CartService.java
+++ src/main/java/com/shopping/service/CartService.java
@@ -2,6 +2,8 @@ package com.shopping.service;
 
 import com.shopping.dto.CartDetailDto;
 import com.shopping.dto.CartItemDto;
+import com.shopping.dto.CartOrderDto;
+import com.shopping.dto.OrderDto;
 import com.shopping.entity.Cart;
 import com.shopping.entity.CartItem;
 import com.shopping.entity.Item;
@@ -28,6 +30,7 @@ public class CartService {
     private final MemberRepository memberRepository;
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final OrderService orderService;
 
     public Long addCart(CartItemDto cartItemDto,String email){
         Item item=itemRepository.findById(cartItemDto.getItemId())
@@ -102,6 +105,32 @@ public class CartService {
     public void deleteCartItem(Long cartItemId){
         CartItem cartItem=cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
         cartItemRepository.delete(cartItem);
+    }
+    
+    //주문 로직으로 전달할 orderDto 리스트 생성 및 주문 로직 호출, 주문한 상품 장바구니에서 제거
+    public Long orderCartItem(List<CartOrderDto> cartOrderDtoList,String email){
+        List<OrderDto> orderDtoList=new ArrayList<>();
+
+        //장바구니 페이지에서 전달받은 주문 상품 번호를 이용하여 주문 로직으로 전달할 orderDto 객체 생성
+        for(CartOrderDto cartOrderDto:cartOrderDtoList){ 
+            CartItem cartItem=cartItemRepository.findById(cartOrderDto.getCartItemId())
+                    .orElseThrow(EntityNotFoundException::new);
+
+            OrderDto orderDto=new OrderDto();
+            orderDto.setItemId(cartItem.getItem().getId());
+            orderDto.setCount(cartItem.getCount());
+            orderDtoList.add(orderDto);
+        }
+
+        Long orderId= orderService.orders(orderDtoList,email); //장바구니에 담은 상품을 주문하도록 주문하도록 주문 로직 호출
+
+        for(CartOrderDto cartOrderDto:cartOrderDtoList){ //주문한 상품들을 장바구니에서 제거
+            CartItem cartItem=cartItemRepository.findById(cartOrderDto.getCartItemId())
+                    .orElseThrow(EntityNotFoundException::new);
+            cartItemRepository.delete(cartItem);
+        }
+
+        return orderId;
     }
 
 }
